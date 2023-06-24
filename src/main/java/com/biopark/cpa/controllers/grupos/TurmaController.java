dbc894bfea6064fe0530814b9c4ec18a6368d64b
Abraao-Junior-Silva <129560230+Abraao-Junior-Silva@users.noreply.github.com>
2023-06-23 22:04:59 -0300

    refactor: clean code

--- src/main/java/com/biopark/cpa/controllers/grupos/TurmaController.java
+++ src/main/java/com/biopark/cpa/controllers/grupos/TurmaController.java
@@ -2,7 +2,6 @@ package com.biopark.cpa.controllers.grupos;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/biopark/cpa/controllers/pessoas/MembroCPAController.java
+++ src/main/java/com/biopark/cpa/controllers/pessoas/MembroCPAController.java
@@ -1,16 +1,12 @@
 package com.biopark.cpa.controllers.pessoas;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.biopark.cpa.dto.GenericDTO;
 import com.biopark.cpa.dto.MembroCPADTO;
-import com.biopark.cpa.entities.user.User;
 import com.biopark.cpa.form.pessoas.CadastroCPA;
 import com.biopark.cpa.services.pessoas.MembrosCPAService;
 
